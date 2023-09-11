c24ff11ed0c2605176c8f6fb33ca4545cefe867a
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-09-11 10:24:26 +0300

    Update testprice.js

--- price-Automation Task-main/cypress/e2e/testprice.js
+++ price-Automation Task-main/cypress/e2e/testprice.js
@@ -1,28 +1,34 @@
 /// <reference types="Cypress" />
 
-describe('Total Old Prices And New Orices', () => {
-    it('find elements', () => {
+describe('Total Old Prices And New Prices', () => {
+    it('calculate total old and new prices', () => {
         cy.visit("https://www.automationteststore.com/")
         
         let totalOldPrice = 0; 
         let totalNewPrice = 0; 
-       
-        cy.get('#special > .container-fluid').find('.pricenew').each(($element) => {
-            
-            const newPrice = parseFloat($element.text().replace("$", ""));
-            cy.log(newPrice);
-            totalNewPrice += newPrice;
-        }).then(() => {
-            cy.get('#special > .container-fluid').find('.priceold').each(($element) => {
-                  const oldPrice = parseFloat($element.text().replace("$", ""));
+
+        cy.get('#special > .container-fluid')
+            .find('.pricenew')
+            .each(($element) => {
+                const newPrice = parseFloat($element.text().replace("$", ""));
+                cy.log(newPrice);
+                totalNewPrice += newPrice;
+            });
+
+        cy.get('#special > .container-fluid')
+            .find('.priceold')
+            .each(($element) => {
+                const oldPrice = parseFloat($element.text().replace("$", ""));
                 cy.log(oldPrice);   
                 totalOldPrice += oldPrice;
-            }).then(() => {
-                const formattedTotalOldPrice = totalOldPrice.toFixed(2);
-                const formattedTotalNewPrice = totalNewPrice.toFixed(2);
-                window.alert("Total Old Price Is $" + formattedTotalOldPrice);
-                window.alert("Total New Price Is $" + formattedTotalNewPrice);
             });
+
+        cy.then(() => {
+            const formattedTotalOldPrice = totalOldPrice.toFixed(2);
+            const formattedTotalNewPrice = totalNewPrice.toFixed(2);
+            cy.log("Total Old Price Is $" + formattedTotalOldPrice);
+            cy.log("Total New Price Is $" + formattedTotalNewPrice);
+            // You can replace the log with other actions if needed.
         });
     });
 });
